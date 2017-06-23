01bcdb560e07210588b3134cdc60e3ee6d5a2254
Hanna Söderström <gothbarbie84@gmail.com>
2017-06-23 14:48:41 +0200

    switched from main to index for entry file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
-const config = {}
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+}
 
 module.exports = config
